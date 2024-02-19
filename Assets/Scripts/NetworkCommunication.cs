8a10d31311e2d1687b95c66e9e192d26aaddb619
aykae <abdrai19@gmail.com>
2024-02-19 18:05:31 -0500

    first commit

---
+++ Assets/Scripts/NetworkCommunication.cs
@@ -0,0 +1,48 @@
+namespace MyFirstARGame
+{
+    using Photon.Pun;
+    using UnityEngine;
+
+    /// <summary>
+    /// You can use this class to make RPC calls between the clients. It is already spawned on each client with networking capabilities.
+    /// </summary>
+    public class NetworkCommunication : MonoBehaviourPun
+    {
+        [SerializeField]
+        private Scoreboard scoreboard;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void IncrementScore()
+        {
+            var playerName = $"Player {PhotonNetwork.LocalPlayer.ActorNumber}";
+            var currentScore = this.scoreboard.GetScore(playerName);
+            this.photonView.RPC("Network_SetPlayerScore", RpcTarget.All, playerName, currentScore + 1);
+        }
+
+        [PunRPC]
+        public void Network_SetPlayerScore(string playerName, int newScore)
+        {
+            Debug.Log($"Player {playerName} scored!");
+            this.scoreboard.SetScore(playerName, newScore);
+        }
+
+        public void UpdateForNewPlayer(Photon.Realtime.Player player)
+        {
+            var playerName = $"Player {PhotonNetwork.LocalPlayer.ActorNumber}";
+            var currentScore = this.scoreboard.GetScore(playerName);
+            this.photonView.RPC("Network_SetPlayerScore", player, playerName, currentScore);
+        }
+    }
+
+}
